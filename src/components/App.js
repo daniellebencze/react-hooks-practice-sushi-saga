42b61becaa8fd9acbad7ea7fb2dfa8c4e7a208d6
Danielle Bencze <daniellebencze@live.com>
2022-01-26 21:00:29 -0800

    commit tree 991bded242bd8c5f0f049b51086790ae67b9a6da

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
+const CONTAINER_SIZE = 4;
+const START_BUDGET = 100;
 
 function App() {
+  const [sushi, setSushi] = useState([]);
+  const [sushiStart, setSushiStart] = useState(0);
+  const [budget, setBudget] = useState(START_BUDGET);
+
+  useEffect(
+    () =>
+      fetch(API)
+        .then((response) => response.json())
+        // console.log("rendered!");
+        .then((json) => setSushi(json)),
+    []
+  );
+
+  function showMore() {
+    setSushiStart(sushiStart + CONTAINER_SIZE);
+  }
+
+  function eatSushi(piece) {
+    // console.log("sushi eaten!", piece);
+    // /piece.eaten=true
+
+    if (piece.eaten || budget - piece.price < 0) {
+      return;
+    }
+
+    setBudget(budget - piece.price);
+
+    setSushi(
+      sushi.map((s) => (s.id === piece.id ? { ...piece, eaten: true } : s))
+    );
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer
+        showMore={showMore}
+        eatSushi={eatSushi}
+        sushi={sushi.slice(sushiStart, sushiStart + CONTAINER_SIZE)}
+      />
+      <Table budget={budget} plates={sushi.filter((p) => p.eaten)} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushi, showMore, eatSushi }) {
   return (
     <div className="belt">
       {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushi.map((s) => (
+        <Sushi key={s.id} sushi={s} handleClick={eatSushi} />
+      ))}
+
+      <MoreButton handleClick={showMore} />
     </div>
   );
 }
